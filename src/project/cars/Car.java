f208df9223cc782198e32ecc2625eed655b7e202
Boris Ribarski <ribarskiboris@gmail.com>
2023-11-23 13:54:09 +0200

    Adding mileage and gradually broking from driving

--- src/project/cars/Car.java
+++ src/project/cars/Car.java
@@ -3,15 +3,20 @@ package project.cars;
 import project.cars.engines.Engine;
 
 public abstract class Car {
+    private static final double broke_coefficient  = 0.001;
     protected static String name;
     private Engine engine;
     private final CarType type;
     private final String model;
+    private double strength;
+    private int mileage;
     public Car(Engine engine, CarType type, String model) {
         setEngine(engine);
         this.type = type;
         this.model = model;
         setName();
+        strength = 100;
+        mileage = 0;
     }
     public String getSpecs(){
         return name + " " + model + " " + type +
@@ -28,4 +33,9 @@ public abstract class Car {
     }
 
     protected abstract void setName();
+
+    public void drive(int km){
+        this.mileage += km;
+        this.strength -= km * broke_coefficient;
+    }
 }
