8462c1ac8f9c0b4d378713addbae74ce8bf9ba74
Boris Ribarski <ribarskiboris@gmail.com>
2023-11-24 01:25:33 +0200

    Remove problems

--- src/project/Main.java
+++ src/project/Main.java
@@ -5,9 +5,6 @@ import project.cars.CarType;
 import project.companies.CarCompany;
 import project.companies.Companies;
 import project.services.RepairService;
-import project.services.Service;
-import project.services.Tunable;
-import project.services.TuneService;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +12,6 @@ public class Main {
         company.setFactory(Companies.BMW);
         Car car1 = company.produceCar("E36", "318", "m40b16", CarType.COUPE);
         RepairService service = new RepairService();
+        car1.repair(service);
     }
 }
