ad4da7862846f905d0fc4b7b9ebea66abbade17c
Boris Ribarski <ribarskiboris@gmail.com>
2023-11-25 21:42:58 +0200

    Remove problems

--- src/project/Main.java
+++ src/project/Main.java
@@ -9,12 +9,15 @@ public class Main {
         City plovdiv = new City("Plovdiv");
         City burgas = new City("Burgas");
         City tarnovo = new City("Tarnovo");
+        City pazardzhik = new City("Pazardzhik");
         CitiesMap map = new CitiesMap();
         map.addRoad(sofia, plovdiv, 130);
         map.addRoad(sofia, tarnovo, 150);
         map.addRoad(burgas, plovdiv, 300);
         map.addRoad(burgas, sofia, 400);
         map.addRoad(tarnovo, burgas, 100);
-        System.out.println(map.find(sofia, burgas));
+        map.addRoad(pazardzhik, sofia, 100);
+        map.addRoad(pazardzhik, tarnovo, 40);
+        System.out.println(map.findShortestRoad(sofia, burgas));
     }
 }
--- src/project/cities/CitiesMap.java
+++ src/project/cities/CitiesMap.java
@@ -16,35 +16,30 @@ public class CitiesMap {
     public void addRoad(City start, City end, int distance){
         addRoad(new Road(start, end, distance));
     }
-    public int find(City start, City end){
+    public int findShortestRoad(City start, City end){
         Set<City> visitedCities = new HashSet<>();
         visitedCities.add(start);
         return findDistance(start, end ,visitedCities, start);
     }
 
-    public int findDistance(City start, City end, Set<City> visitedCities, City constStart){
+    private int findDistance(City start, City end, Set<City> visitedCities, City constStart){
         int distance = 0;
         int shortest = -1;
         for(Road road : map){
             if(road.getStart() == start){
-                System.out.print(road + " ");
                 if (visitedCities.contains(road.getEnd())) {
-                    System.out.println("Don't go back");
                     continue;
                 } else if (road.getEnd() == end) {
-                    System.out.println("Find end");
-                    if(start == constStart){
-                        if(shortest > road.getDistance()){
-                            return shortest ;
-                        }
+                    if (start != constStart){
+                        return road.getDistance();
                     }
-                    return road.getDistance();
+                    else if(shortest > road.getDistance() || shortest == -1){
+                        shortest = road.getDistance();
+                    }
                 } else {
-                    System.out.println("driving");
                     visitedCities.add(road.getEnd());
                     distance += findDistance(road.getEnd(), end, visitedCities, constStart);
                     distance += road.getDistance();
-                    System.out.println(distance + " " + shortest);
                     if(shortest > distance || shortest == -1){
                         shortest = distance;
                     }
@@ -52,7 +47,6 @@ public class CitiesMap {
                 }
             }
         }
-        System.out.println("end  " + shortest);
         return shortest;
     }
 }
